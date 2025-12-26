15d8e57191166a48ef6b2f089842d7b68c91f6e7
Yash Chougale <yashachougale2001@gmail.com>
2025-12-26 20:21:41 +0530

    Updated git Ignore and Main

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from generator.story_generator import generate_story
 
 def main():
     # file_path = "examples/sample_git_log.txt"  # Example input file
-    file_path = "examples/my_commits.txt"  # Example input file
+    file_path = "examples/my_commits.txt"  # My Own GIT commit input file
     commit_lines = load_commits_from_file(file_path)
     commits = parse_commits(commit_lines)
     phases = analyze_commits(commits)
